cc647a25e032f4f314813224920943221c411fb3
Alexander Sopov <alexander.sopov@schibsted.com@A120890-NO.local>
2024-12-12 13:42:56 +0100

    adds envmap and reflective surfaces

--- src/prefabs/towers/SimpleTower.js
+++ src/prefabs/towers/SimpleTower.js
@@ -1,97 +1,111 @@
-import * as THREE from 'three'
-import Selectable from '../../components/Selectable'
-import Tower from './Tower'
-import pisa from './images/pisa.jpg'
-import upgrade from './images/upgrade.jpg'
-import sell from './images/sell.jpg'
-import { subscribe } from '../../utilities/EventBus'
+import * as THREE from "three";
+import Selectable from "../../components/Selectable";
+import Tower from "./Tower";
+import pisa from "./images/pisa.jpg";
+import upgrade from "./images/upgrade.jpg";
+import sell from "./images/sell.jpg";
+import { subscribe } from "../../utilities/EventBus";
 
-const SIZE = 1.5
-const color = 0xa4444a
-const eventBusPrefix = 'sellSimpleTower_'
+const SIZE = 1.5;
+const color = 0xa4444a;
+const eventBusPrefix = "sellSimpleTower_";
 export class SimpleTower extends Tower {
+  constructor(scene, camera, opts) {
+    super();
 
-  constructor (scene, camera, opts) {
-    super()
+    const { helpers, basePosition } = opts ?? {};
 
-    const { helpers, basePosition } = opts ?? {}
+    this.scene = scene;
+    this.camera = camera;
 
-    this.scene = scene
-    this.camera = camera
+    const geometry = new THREE.CylinderGeometry(
+      SIZE * 1,
+      SIZE * 1.5,
+      SIZE * 4,
+      16
+    );
+    const material = new THREE.MeshStandardMaterial({
+      color,
+      metalness: 1,
+      roughness: 0,
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.cube.position.y = 1;
+    this.cube.castShadow = true;
+    this.add(this.cube);
 
-    const geometry = new THREE.CylinderGeometry( SIZE * 1, SIZE * 1.5, SIZE * 4, 16 ); 
-    const material = new THREE.MeshStandardMaterial( { color } );
-    this.cube = new THREE.Mesh( geometry, material );
-    this.cube.position.y = 1
-    this.cube.castShadow = true
-    this.add( this.cube );
-
-    this.components.push(new Selectable(this, this.cube, color, this.generateCommandPanelData()))
+    this.components.push(
+      new Selectable(this, this.cube, color, this.generateCommandPanelData())
+    );
 
     if (helpers) {
-      this.add( createPlaneGeo() );
+      this.add(createPlaneGeo());
     }
 
     if (basePosition) {
-      this.position.x = basePosition.x
-      this.position.y = basePosition.y
-      this.position.z = basePosition.z
+      this.position.x = basePosition.x;
+      this.position.y = basePosition.y;
+      this.position.z = basePosition.z;
     }
 
-    this.scene.add(this)
-    subscribe(eventBusPrefix + this.id, price => this.sell(price))
+    this.scene.add(this);
+    subscribe(eventBusPrefix + this.id, (price) => this.sell(price));
   }
 
-  generateCommandPanelData () {
+  generateCommandPanelData() {
     return {
       portrait: {
         image: pisa,
         animation: false,
         health: {
           current: 300,
-          max: 300
+          max: 300,
         },
         mana: {
           current: 100,
-          max: 100
+          max: 100,
         },
       },
       stats: {
-        name: 'SimpleTower',
+        name: "SimpleTower",
         stats: [
           {
-            label: 'Armor',
+            label: "Armor",
             value: 0,
-            valueLabel: '0'
-          }, {
-            label: 'Damage',
+            valueLabel: "0",
+          },
+          {
+            label: "Damage",
             range: [4, 6],
-            valueLabel: '4 - 6'
-          }, {
-            label: 'Speed',
+            valueLabel: "4 - 6",
+          },
+          {
+            label: "Speed",
             value: 1,
-            valueLabel: 'Average'
-          }, {
-            label: 'Range',
+            valueLabel: "Average",
+          },
+          {
+            label: "Range",
             value: 60,
-            valueLabel: '60'
-          }
+            valueLabel: "60",
+          },
         ],
       },
       options: [
         {
           icon: upgrade,
-          description: 'Upgrade tower to StrongSimpleTower.',
+          description: "Upgrade tower to StrongSimpleTower.",
           cost: 400,
-          costLabel: 'Buy'
-        }, {
+          costLabel: "Buy",
+        },
+        {
           icon: sell,
-          description: 'Sell tower to regain money.',
+          description: "Sell tower to regain money.",
           cost: 200,
-          costLabel: 'Sell',
-          action: [eventBusPrefix + this.id, 200]
-        }
-      ]
-    }
+          costLabel: "Sell",
+          action: [eventBusPrefix + this.id, 200],
+        },
+      ],
+    };
   }
 }
--- src/scene-manager.js
+++ src/scene-manager.js
@@ -1,133 +1,149 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-import { SimpleTower } from './prefabs/towers/SimpleTower';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
-import { LuminosityShader } from 'three/addons/shaders/LuminosityShader.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { KeyboardControls } from './controls/KeyboardControls'
-import MouseControls from './controls/MouseControls'
-import TowerBuilders from './builders/TowerBuilders';
+import { SimpleTower } from "./prefabs/towers/SimpleTower";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
+import { LuminosityShader } from "three/addons/shaders/LuminosityShader.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { KeyboardControls } from "./controls/KeyboardControls";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
+import MouseControls from "./controls/MouseControls";
+import TowerBuilders from "./builders/TowerBuilders";
 
-const sceneSubjects = []
+const sceneSubjects = [];
 
 export const removeSceneSubject = (ch) => {
-  const i = sceneSubjects.findIndex(ss => ss.id == ch.id)
-  if (i < 0)
-    throw new Error("Object not a subject of scene")
-  sceneSubjects.splice(i, 1)
-}
-export const addSceneSubject = (ss) => sceneSubjects.push(ss)
-export const getSceneSubjects = () => sceneSubjects
-
+  const i = sceneSubjects.findIndex((ss) => ss.id == ch.id);
+  if (i < 0) throw new Error("Object not a subject of scene");
+  sceneSubjects.splice(i, 1);
+};
+export const addSceneSubject = (ss) => sceneSubjects.push(ss);
+export const getSceneSubjects = () => sceneSubjects;
 
 const initScene = (body) => {
-  const canvas = body
+  const canvas = body;
 
-  var scene,
-    composer,
-    renderer,
-    camera
+  var scene, composer, renderer, camera;
+  const rgbeLoader = new RGBELoader();
 
   const buildScene = () => {
     scene = new THREE.Scene();
-    const texture = new THREE.TextureLoader().load( "textures/background.png" );
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 1, 1 );
-    scene.background = texture
-    // scene.background = new THREE.Color( 0xas0a0a0 );
-    scene.fog = new THREE.Fog( 0xa0a0a0, 10, 250 );
-  }
+    scene.fog = new THREE.Fog(0xa0a0a0, 10, 250);
+  };
+
+  const applyEnvMap = async () => {
+    const envMap = await rgbeLoader.loadAsync("/textures/1k.hdr");
+    envMap.mapping = THREE.EquirectangularReflectionMapping;
+    renderer.toneMappingExposure = 0.65;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    // THREE.NoToneMapping
+    // THREE.LinearToneMapping
+    // THREE.ReinhardToneMapping
+    // THREE.CineonToneMapping
+    scene.background = envMap;
+    scene.environment = envMap;
+    scene.environmentIntensity = 0.5;
+    // environmentMap.rotation.y = Math.PI * 0.5
+    scene.environmentRotation = new THREE.Euler(0, Math.PI / -2, 0, "XYZ");
+    scene.backgroundRotation = new THREE.Euler(0, Math.PI / -2, 0, "XYZ");
+  };
 
   const buildCamera = () => {
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.z = 50
-    camera.position.x = 4
-    camera.position.y = 40
-  }
+    camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 50;
+    camera.position.x = 4;
+    camera.position.y = 40;
+  };
 
   const buildRender = () => {
     renderer = new THREE.WebGLRenderer();
     renderer.shadowMap.enabled = true;
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    canvas.appendChild( renderer.domElement );
-    const luminosityPass = new ShaderPass( LuminosityShader );
-    composer = new EffectComposer( renderer );
-    composer.addPass( luminosityPass );
-    const renderPass = new RenderPass( scene, camera );
-    composer.addPass( renderPass );
-  }
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    canvas.appendChild(renderer.domElement);
+    const luminosityPass = new ShaderPass(LuminosityShader);
+    composer = new EffectComposer(renderer);
+    composer.addPass(luminosityPass);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+  };
 
-  function createSceneSubjects () {
-    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x8d8d8d, 3 );
-    hemiLight.position.set( 0, 20, 0 );
-    scene.add( hemiLight );
-    
-    const floor = new THREE.Mesh( new THREE.PlaneGeometry( 500, 500 ), new THREE.MeshPhongMaterial( { color: 0xcbcbcb, depthWrite: false } ) );
-    floor.rotation.x = - Math.PI / 2;
+  function createSceneSubjects() {
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 3);
+    hemiLight.position.set(0, 20, 0);
+    scene.add(hemiLight);
+
+    const floor = new THREE.Mesh(
+      new THREE.PlaneGeometry(500, 500),
+      new THREE.MeshStandardMaterial({
+        color: 0xcbcbcb,
+        depthWrite: false,
+        metalness: 0,
+        roughness: 1,
+      })
+    );
+    floor.rotation.x = -Math.PI / 2;
     floor.receiveShadow = true;
-    floor.isFloor = true
-    scene.add( floor );
-    floor.update = () => {}
-    sceneSubjects.push(floor)
-    
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
-    dirLight.position.set( 3, 10, 10 );
+    floor.isFloor = true;
+    scene.add(floor);
+    floor.update = () => {};
+    sceneSubjects.push(floor);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight.position.set(3, 10, 10);
     dirLight.castShadow = true;
     dirLight.shadow.camera.top = 2;
-    dirLight.shadow.camera.bottom = - 2;
-    dirLight.shadow.camera.left = - 2;
+    dirLight.shadow.camera.bottom = -2;
+    dirLight.shadow.camera.left = -2;
     dirLight.shadow.camera.right = 2;
     dirLight.shadow.camera.near = 0.1;
     dirLight.shadow.camera.far = 40;
-    scene.add( dirLight );
+    scene.add(dirLight);
 
+    sceneSubjects.push(new KeyboardControls(camera, renderer.domElement));
+    sceneSubjects.push(new MouseControls(scene, camera, renderer.domElement));
+
+    sceneSubjects.push(new SimpleTower(scene, camera));
     sceneSubjects.push(
-      new KeyboardControls( camera, renderer.domElement )
-    )
+      new SimpleTower(scene, camera, {
+        basePosition: new THREE.Vector3(8, 0, 8),
+      })
+    );
     sceneSubjects.push(
-      new MouseControls( scene, camera, renderer.domElement )
-    )
-      
+      new SimpleTower(scene, camera, {
+        basePosition: new THREE.Vector3(0, 0, 8),
+      })
+    );
     sceneSubjects.push(
-      new SimpleTower( scene, camera )
-    )
-    sceneSubjects.push(
-      new SimpleTower( scene, camera, { basePosition: new THREE.Vector3(8, 0, 8) } )
-    )
-    sceneSubjects.push(
-      new SimpleTower( scene, camera, { basePosition: new THREE.Vector3(0, 0, 8) } )
-    )
-    sceneSubjects.push(
-      new SimpleTower( scene, camera, { basePosition: new THREE.Vector3(8, 0, 0) } )
-    )
-    sceneSubjects.push(
-      new TowerBuilders(scene, camera)
-    )
+      new SimpleTower(scene, camera, {
+        basePosition: new THREE.Vector3(8, 0, 0),
+      })
+    );
+    sceneSubjects.push(new TowerBuilders(scene, camera));
     // scene.add(EnemyCharacter({
     //   // helpers: true,
     //   // basePosition: new THREE.Vector3(0, 0, 0)
     // }))
-
   }
 
   function update(d) {
-    for(let i=0; i<sceneSubjects.length; i++)
-      sceneSubjects[i].update(d);  
+    for (let i = 0; i < sceneSubjects.length; i++) sceneSubjects[i].update(d);
     composer.render();
   }
 
   buildScene();
   buildCamera();
   buildRender();
+  applyEnvMap();
   createSceneSubjects();
-
-
 
   return {
     update,
-    onWindowResize: evt => {
+    onWindowResize: (evt) => {
       const { clientWidth, clientHeight } = canvas;
       // screenDimensions.clientWidth = clientWidth;
       // screenDimensions.clientHeight = clientHeight;
@@ -135,21 +151,20 @@ const initScene = (body) => {
       composer.setSize(clientWidth, clientHeight);
       camera.aspect = clientWidth / clientHeight;
       camera.updateProjectionMatrix();
-    }
-  }
-}
+    },
+  };
+};
 
 export default class SceneManager {
-  scene
-  constructor (canvas) {
-    this.scene = initScene(canvas)
+  scene;
+  constructor(canvas) {
+    this.scene = initScene(canvas);
   }
 
   update = (d) => {
-    this.scene.update(d)
-  }
+    this.scene.update(d);
+  };
   onWindowResize = (d) => {
-    this.scene.onWindowResize(d)
-  }
+    this.scene.onWindowResize(d);
+  };
 }
-
